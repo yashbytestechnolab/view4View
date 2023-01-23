720b5da181c94539f8a59443a21f691c9e229b0b
Yash Sodha Bytes <yash.sodha@project10k.com>
2023-01-23 18:20:38 +0530

    fix: ios in app purchase

--- source/modules/EarnCoin/BuyCoin/BuyCoin.tsx
+++ source/modules/EarnCoin/BuyCoin/BuyCoin.tsx
@@ -11,12 +11,7 @@ import { EarnCoin } from '../../../services';
 import { type as keys } from '../../../constants/types';
 import { showMessage } from 'react-native-flash-message';
 import { useNavigation } from '@react-navigation/native';
-
-
-const itemSkus: any = Platform.select({
-    ios: ["1KCoins", "2.5KCoins", "5KCoins"],
-    android: ["1kcoins", "2.5kcoins", "5kcoins"]
-});
+import { getItems, initilizeIAPConnection, onGetCoinAmount, onPurchase } from '../../../services/InAppPurchaseServices';
 
 let purchaseUpdateSubscription: any = null;
 let purchaseErrorSubscription: any = null;
@@ -29,55 +24,17 @@ export const BuyCoin = () => {
     const navigation = useNavigation();
 
     useEffect(() => {
-        initilizeIAPConnection();
+        const isConnectedIAP: any = initilizeIAPConnection();
+        if (isConnectedIAP) {
+            let products = getItems()
+            getProducts(products)
+        }
     }, []);
-
-    const initilizeIAPConnection = async () => {
-        await RNIap.initConnection()
-            .then(async (connection) => {
-                // console.log('IAP result', connection);
-                connection && getItems();
-            })
-            .catch((err) => {
-                // console.warn(`IAP ERROR ${err.code}`, err.message);
-                showMessage({
-                    type: 'danger',
-                    message: err?.message
-                })
-            });
-
-        if (Platform.OS === 'android') {
-            await RNIap.flushFailedPurchasesCachedAsPendingAndroid()
-                .then(async (consumed) => {
-                    console.log('consumed all items?', consumed);
-                }).catch((err) => {
-                    console.warn(`flushFailedPurchasesCachedAsPendingAndroid ERROR ${err.code}`, err.message);
-                });
-        }
-    };
-
-    const getItems = async () => {
-        try {
-            const Products = await RNIap.getProducts({ skus: itemSkus });
-            // console.log(' IAP Su', Products);
-            if (Products?.length !== 0) {
-                getProducts(Products)
-            }
-        } catch (err: any) {
-            console.warn("IAP error", err?.code, err?.message, err);
-            showMessage({
-                type: 'danger',
-                message: err?.message
-            })
-        }
-    };
 
     useEffect(() => {
         purchaseUpdateSubscription = RNIap.purchaseUpdatedListener(
             async (purchase: any) => {
-                console.log("purchase", purchase);
                 const receipt = Platform.OS === 'ios' ? purchase?.transactionReceipt : purchase?.purchaseToken;
-                console.log("receipt", receipt);
                 if (receipt) {
                     try {
                         if (Platform.OS === 'ios') {
@@ -91,7 +48,7 @@ export const BuyCoin = () => {
                             await RNIap?.finishTransaction({ purchase });
                         }
                     } catch (ackErr: any) {
-                        console.log('ackErr INAPP>>>>', ackErr);
+                        // console.log('ackErr INAPP>>>>', ackErr);
                         if (Platform.OS === 'ios') {
                             await RNIap.clearTransactionIOS();
                             await RNIap.clearProductsIOS()
@@ -131,23 +88,11 @@ export const BuyCoin = () => {
         });
     }, []);
 
-    const onPurchase = async (sku: string) => {
-        try {
-            RNIap.requestPurchase({
-                sku,
-                andDangerouslyFinishTransactionAutomaticallyIOS: false,
-            })
-        } catch (err: any) {
-            console.warn(err.code, err.message);
-        }
-    };
-
     const onRewardCoins = async (rewardId: any) => {
         let redeemCoin: any = await onGetCoinAmount(rewardId);
         if (redeemCoin) {
             await EarnCoin(getBalance)?.then((res) => {
                 dispatchCoin({ types: keys.GET_CURRENT_COIN, payload: getBalance + redeemCoin })
-
                 showMessage({
                     message: `${redeemCoin} coins credited`,
                     type: 'success',
@@ -165,27 +110,6 @@ export const BuyCoin = () => {
 
     }
 
-    const onGetCoinAmount = async (rewardId: any) => {
-        console.log("rewardId", rewardId);
-
-        switch (rewardId) {
-            case '1kcoins':
-                return 1000
-            case '1KCoins':
-                return 1000
-            case '2.5kcoins':
-                return 2500
-            case '2.5KCoins':
-                return 2500
-            case '5KCoins':
-                return 5000
-            case '5kcoins':
-                return 5000
-            default:
-                return 0
-        }
-    }
-
 
 
     const reanderCard = (color: string) => {
@@ -196,7 +120,7 @@ export const BuyCoin = () => {
                 <Text style={[F40014?.main, style.cardMultiText]}>
                     {multitext}
                 </Text>
-                <TouchableOpacity style={style.cardAmountWrapper} onPress={() => onPurchase("5KCoins")}>
+                <TouchableOpacity style={style.cardAmountWrapper} onPress={() => onPurchase("2.5KCoins")}>
                     <Text style={F60016?.textStyle, F60016?.semiBolt}>$8.34</Text>
                 </TouchableOpacity>
 
