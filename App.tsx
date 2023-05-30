0d930bd587bcfe9fbfb9b926d94b873dc0ad3c29
rita.vaja <rita.vaja@bytestechnolab.com>
2023-05-30 12:36:09 +0530

    fix:issue updata

--- App.tsx
+++ App.tsx
@@ -132,7 +132,7 @@ export default function App() {
           isInternetBack={isInternetBack}
           setIsInternetBack={setIsInternetBack} />
         <NavigationContainer theme={MyTheme}  >
-          {!updateAlert ?
+          {updateAlert ?
             <InviteFriend notifyUpdate={updateAlert} /> :
             <>
               <RootNavigation />
